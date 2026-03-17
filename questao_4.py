7ad3f5bfb62ce4cb73aa0b4eb4e19cb6f8bf436f
João Pedro <127751417+jpfirmo@users.noreply.github.com>
2026-03-17 20:42:37 -0300

    idade em dias

---
+++ questao_4.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+
+idade = int(input("Digite sua idade: "))
+
+dias_vividos = idade * 365
+
+print(f"Olá {nome}, você já viveu aproximadamente {dias_vividos} dias.")
