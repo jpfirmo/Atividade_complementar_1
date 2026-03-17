9118d3599e4490ce229797212b51b981d05fcab6
João Pedro <127751417+jpfirmo@users.noreply.github.com>
2026-03-17 20:20:58 -0300

    conversor de medida

---
+++ questao_1.py
@@ -0,0 +1,7 @@
+medida = float(input("Digite a medida em metros: "))
+
+centimetros = medida * 100
+milimetros = medida * 1000
+
+print(f"A medida em centímetros é: {centimetros} cm")
+print(f"A medida em milímetros é: {milimetros} mm")
