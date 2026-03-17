c5836810435abe48e1ebc414006fda9602dfcc15
João Pedro <127751417+jpfirmo@users.noreply.github.com>
2026-03-17 20:39:42 -0300

    analisador de texto

---
+++ questao_3.py
@@ -0,0 +1,8 @@
+frase = input("Digite uma frase: ")
+
+quantidade_caracteres = len(frase)
+
+tipo_dado = type(frase)
+
+print("Quantidade de caracteres:", quantidade_caracteres)
+print("Tipo do dado:", tipo_dado)
