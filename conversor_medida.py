93511f5d0cb1fa6c687d445c551992245eeec45f
jpfirmo <127751417+jpfirmo@users.noreply.github.com>
2026-03-17 20:32:47 -0300

    Delete conversor_medida.py

--- conversor_medida.py
+++
@@ -1,7 +0,0 @@
-medida = float(input("Digite a medida em metros: "))
-
-centimetros = medida * 100
-milimetros = medida * 1000
-
-print(f"A medida em centímetros é: {centimetros} cm")
-print(f"A medida em milímetros é: {milimetros} mm")
