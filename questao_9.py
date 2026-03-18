f53929db859ae65d15df96de878cfdf1bf331cf8
João Pedro <127751417+jpfirmo@users.noreply.github.com>
2026-03-17 23:04:22 -0300

    validador de senha

---
+++ questao_9.py
@@ -0,0 +1,8 @@
+while True:
+    senha = input("Digite a senha: ")
+
+    if senha == "python123":
+        print("Acesso Permitido")
+        break
+    else:
+        print("Senha incorreta. Tente novamente.")
